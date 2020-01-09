c51898ed9b0d5c5ea003ec44ed85f6fcf3fa5c84
Boris Manashirov <boris200054@gmail.com>
2020-01-09 14:08:56 +0200

    Change forecast description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,8 +10,9 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find location. PLease try again', undefined)
         } else {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' +
-            body.currently.temperature + ' degrees out. There is a ' +
-            body.currently.precipProbability * 100 + '% chance of rain')
+            body.currently.temperature + ' degrees outside. There is a ' +
+            body.currently.precipProbability * 100 + '% chance of rain. Today\'s high: ' + 
+            body.daily.data[0].temperatureHigh + ' degrees. Today\'s low: ' + body.daily.data[0].temperatureLow + ' degrees')
         }
     })
 }
